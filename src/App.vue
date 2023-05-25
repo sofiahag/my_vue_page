b816a939be5f816565d87040193a3192c84d3d2e
sofia.haglund <sofia.haglund@scilifelab.se>
2023-05-25 22:26:37 +0200

    first version

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import MainContent from './components/MainContent.vue'
+import CodeFooter from './components/CodeFooter.vue'
+</script>
+
+<template>
+  <main>
+    <div class="content">
+      <NavBar/>
+      <MainContent/>
+      <CodeFooter/>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+.content {
+  text-align: center;
+  margin: 0;
+} 
+
+</style>
